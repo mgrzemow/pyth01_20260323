e9b14c3df55ec93fb76f25e91f7065032aed8956
Michał Grzemowski <mgrzemow@gmail.com>
2026-03-25 13:56:07 +0100

    c16

--- c16.py
+++ c16.py
@@ -12,3 +12,24 @@ Podpowiedzi:
  - nazwę domeny można wyciąć z adresu znajdując znak @
  - dla zapewnienia unikalności - użyjemy zbioru do wczytanych adresów
 """
+if __name__ == '__main__':
+    zbior_maili = set()
+    with open('emails.txt', 'r', encoding='utf8') as f:
+        for linia in f:
+            # wyczyścić adres
+            linia = linia.strip()
+            # sprawdzić czy poprawny
+            if '@' in linia:
+                # dodać do zbioru
+                zbior_maili.add(linia.lower())
+
+    print(zbior_maili)
+    # dla każdego adresu ze zbioru:
+    for email in zbior_maili:
+        # otowrzyć plik domena.txt w trybie at
+        domena = email.split('@')[1]
+
+        with open(domena + '.txt', 'at', encoding='utf8') as f:
+            # dopisać adres
+            f.write(email + '\n')
+
