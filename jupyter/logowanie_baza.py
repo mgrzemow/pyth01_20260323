f6ef01ef1047b689afb7b2a03d9a1429d53dff1f
Michał Grzemowski <mgrzemow@gmail.com>
2026-03-27 15:31:40 +0100

    pandas, bazy

---
+++ jupyter/logowanie_baza.py
@@ -0,0 +1,6 @@
+# Sluchacz1 .. Sluchacz12
+# Sluchacz1_passwd .. Sluchacz12_passwd
+# Postaram sie do końca tygodnia szkolenia utrzymać bazę na chodzie.
+user = 'System'
+password = 'passwd'
+dsn = 'localhost:1521/xe'
